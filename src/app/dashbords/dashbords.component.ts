076e4d184785a4b7537eea8c46d7ce4778eb37ff
lahyanimounir <lahyani.mo@gmail.com>
2019-08-14 16:33:50 +0100

    14/08 (2)

--- src/app/dashbords/dashbords.component.ts
+++ src/app/dashbords/dashbords.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-dashbords',
@@ -10,6 +11,35 @@ export class DashbordsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+  	  	          	          var stackedBar = new Chart('ippm', {
+    type: 'line',
+    data: {
+    	labels: ["44",
+					"0",
+					"6",
+					"22",
+					"10",
+					"13",
+					"40"],
+			datasets: [{
+				label: 'IPPM',
+				backgroundColor: '#009efb',
+				borderColor: '#009efb',
+				fill: false,
+				data: [
+					44,
+					0,
+					6,
+					22,
+					10,
+					13,
+					40
+				],
+			}
+			
+    ]}
+});
   }
 
 }
--- src/app/errors-analysis/errors-analysis.component.ts
+++ src/app/errors-analysis/errors-analysis.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Chart } from 'chart.js';
 @Component({
   selector: 'app-errors-analysis',
   templateUrl: './errors-analysis.component.html',
@@ -14,13 +14,13 @@ export class ErrorsAnalysisComponent implements OnInit {
   	          var stackedBar = new Chart('totalErreors', {
     type: 'line',
     data: {
-    	labels: [44,
-					0,
-					6,
-					22,
-					10,
-					13,
-					40],
+    	labels: ["44",
+					"0",
+					"6",
+					"22",
+					"10",
+					"13",
+					"40"],
 			datasets: [{
 				label: 'line 1',
 				backgroundColor: '#009efb',
@@ -49,39 +49,20 @@ export class ErrorsAnalysisComponent implements OnInit {
 					10],
 			},
 			
-    },
-    options: {
-					responsive: true,
-					scales: {
-						xAxes: [{
-							display: true,
-							scaleLabel: {
-								display: true,
-								labelString: 'Seconds'
-							}
-						}],
-						yAxes: [{
-							display: true,
-							scaleLabel: {
-								display: true,
-								labelString: 'Number Of Errors'
-							}
-						}]
-					}
-				}
+    ]}
 });
 
 
   	          	          var stackedBar = new Chart('testedHarness', {
     type: 'line',
     data: {
-    	labels: [44,
-					0,
-					6,
-					22,
-					10,
-					13,
-					40],
+    	labels: ["44",
+					"0",
+					"6",
+					"22",
+					"10",
+					"13",
+					"40"],
 			datasets: [{
 				label: 'line 1',
 				backgroundColor: '#009efb',
@@ -110,6 +91,56 @@ export class ErrorsAnalysisComponent implements OnInit {
 					10],
 			},
 			
+    ]}
+});
+
+
+  	          	                    var stackedBar = new Chart('electicaltestEroor', {
+    type: 'bar',
+    data: {
+    	labels: ["44",
+					"0",
+					"6",
+					"22",
+					"10",
+					"13",
+					"40"],
+			datasets: [{
+				label: 'errors',
+				backgroundColor: '#009efb',
+				borderColor: '#009efb',
+				fill: false,
+				data: [50,
+					30,
+					16,
+					2,
+					12,
+					15,
+					10],
+			},
+			// {
+			// 	label: 'line 2',
+			// 	backgroundColor: '#72d78efa',
+			// 	borderColor: '#72d78efa',
+			// 	fill: false,
+			// 	type: 'line',
+			// 	data: this.solvingTimeByCounterpart.co,
+			// },
+			{
+				type:"line",
+				label: 'Cumulative',
+				backgroundColor: '#ff0000',
+				borderColor: '#ff0000',
+				fill: false,
+				data: [50,
+					30,
+					16,
+					2,
+					12,
+					15,
+					10],
+			}
+			]
     },
     options: {
 					responsive: true,
@@ -118,14 +149,14 @@ export class ErrorsAnalysisComponent implements OnInit {
 							display: true,
 							scaleLabel: {
 								display: true,
-								labelString: 'Seconds'
+								labelString: 'Electical test Errors'
 							}
 						}],
 						yAxes: [{
 							display: true,
 							scaleLabel: {
 								display: true,
-								labelString: 'Number Of Errors'
+								labelString: 'Errors'
 							}
 						}]
 					}
