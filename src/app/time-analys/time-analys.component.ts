a9edf42f6326334867aa8c5c381a97847ad0912b
said15904 <said.amoursou@gmail.com>
2019-08-18 03:25:49 +0200

    18/08/2019

--- src/app/time-analys/time-analys.component.ts
+++ src/app/time-analys/time-analys.component.ts
@@ -14,9 +14,10 @@ avgErrorType = {Moy : [],Type : []};
 errorSolvingTime = {total : [],TimeU : []};
 solvingTimeByCounterpart = {counterparts : [],count : [],percent : []};
 solvingTimeByWire = {wires : [],count : [],percent : []};
+solvingTimeByCPV = {connectors : [],count : [],percent : []};
   constructor(private httpClient: HttpClient) { }
 
-  
+  ///.get<any[]>('http://localhost/industriel/api/error/read.php')
 getData() {
     this.httpClient
       .get<any[]>('http://api.sunrise-pro.com/error/read.php')
@@ -32,6 +33,8 @@ getData() {
           	this.avgErrorType.Moy.push(response.avgTimeError[i].Moy);
           	this.avgErrorType.Type.push(response.avgTimeError[i].Type);
           }
+
+          response.errorSolvingTime.sort((a, b) => (Number(a.total) > Number(b.total)) ? -1 : 1)
 
           for(let i = 0;i<response.errorSolvingTime.length;i++){
           	this.errorSolvingTime.total.push(response.errorSolvingTime[i].total);
@@ -65,8 +68,66 @@ getData() {
           }
 
 
+          for(let i = 0;i<response.solvingTimeByCPV.length;i++){
+          	this.solvingTimeByCPV.connectors.push(response.solvingTimeByCPV[i].CPV);
+          	this.solvingTimeByCPV.count.push(response.solvingTimeByCPV[i].total);
+          }
+          let sumCPV = this.solvingTimeByCPV.count.reduce(function(a,b){
+			    return Number(a) + Number(b)
+			  }, 0);
+          for(let i = 0;i<response.solvingTimeByCPV.length;i++){
+          	let calc : any= response.solvingTimeByCPV[i].total*100/sumCPV;
+          	calc = calc.toFixed(2);
+          	this.solvingTimeByCPV.percent.push(Number(calc));
+          }
 
-          var stackedBar = new Chart('connectors', {
+
+
+          var connectors = new Chart('connectors', {
+    type: 'bar',
+    data: {
+    	labels: this.solvingTimeByCPV.connectors,
+			datasets: [{
+				label: 'line 1',
+				backgroundColor: '#009efb',
+				borderColor: '#009efb',
+				fill: false,
+				data: this.solvingTimeByCPV.percent,
+				type : 'line'
+			},
+			{
+				label: 'line 3',
+				backgroundColor: '#ff0000',
+				borderColor: '#ff0000',
+				fill: false,
+				data: this.solvingTimeByCPV.count,
+			}
+			]
+    },
+    options: {
+					responsive: true,
+					scales: {
+						xAxes: [{
+							display: true,
+							scaleLabel: {
+								display: true,
+								labelString: 'Seconds'
+							}
+						}],
+						yAxes: [{
+							display: true,
+							scaleLabel: {
+								display: true,
+								labelString: 'Number Of Errors'
+							}
+						}]
+					}
+				}
+});
+
+
+
+          var wires = new Chart('wires', {
     type: 'bar',
     data: {
     	labels: this.solvingTimeByWire.wires,
@@ -76,6 +137,7 @@ getData() {
 				borderColor: '#009efb',
 				fill: false,
 				data: this.solvingTimeByWire.percent,
+				type : 'line'
 			},
 			// {
 			// 	label: 'line 2',
@@ -135,6 +197,7 @@ getData() {
 				borderColor: '#009efb',
 				fill: false,
 				data: this.solvingTimeByCounterpart.percent,
+				type : 'line'
 			},
 			// {
 			// 	label: 'line 2',
